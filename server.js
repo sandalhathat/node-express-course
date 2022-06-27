5c89550cd757fe75c1f0d5eb8e28d39edf0b1422
sandalhathat <sandalhathat@gmail.com>
2022-06-27 11:06:48 -0500

    added first GET route

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+]
 
-
+app.get('/users',function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. nice.',
+        users: mockUserData
+    })
+})
 
 
 
